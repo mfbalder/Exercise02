cbe9b83a711280af387686ffa0e9b5e101ce9097
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 11:53:15 -0700

    Initial commit

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+# In the file arithmetic.py, these function signatures are required
+
+# add(int, int) -> int
+# Returns the sum of the two input integers
+
+# subtract(int, int) -> int
+# Returns the second number subtracted from the first
+
+# multiply(int, int) -> int
+# Multiplies the two inputs together
+
+# divide(int, int) -> float
+# Divides the first input by the second, returning a floating point
+
+# square(int) -> int
+# Returns the square of the input
+
+# cube(int) -> int
+# Returns the cube of the input
+
+# power(int, int) -> int
+# Raises the first integer to the power of the second integer and returns the value.
+
+# mod(int, int) -> int
+# Returns the remainder when the first integer is divided by the second integer.
