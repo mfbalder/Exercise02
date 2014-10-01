5849425de8c125b4d2ae7d5f061f14415785cf5a
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 12:06:32 -0700

    Inserted starter code. Wrote first three functions.

--- arithmetic.py
+++ arithmetic.py
@@ -23,3 +23,30 @@
 
 # mod(int, int) -> int
 # Returns the remainder when the first integer is divided by the second integer.
+
+def add(num1, num2):
+    """Returns the sum of the two input integers"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Returns the second number subtracted from the first"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Multiplies the two inputs together"""
+    return num1 * num2
+
+def divide(num1, num2):
+    pass
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
